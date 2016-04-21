d599f85f34512cfb5135a70f46a4daa5ea506c36
Sergii Getmanchuk <getser79@gmail.com>
2016-04-21 17:49:14 +0300

    my angularjs project template

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angUsersdb')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angUsersdb', ['restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
